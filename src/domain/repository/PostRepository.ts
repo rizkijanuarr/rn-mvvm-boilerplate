085baabeca3b070c7fc490f03fd68e1ef99ef9ab
rizkijanuari <rizkijanuar.work@gmail.com>
2026-03-03 16:12:22 +0700

    FEAT[.]: .

--- src/domain/repository/PostRepository.ts
+++
@@ -1,8 +0,0 @@
-// Domain Repository: INTERFACE
-
-import type {PostResponse} from '../model/PostModel';
-
-export interface PostRepository {
-  getPosts(): Promise<PostResponse>;
-  createPost(body: any): Promise<any>;
-}
